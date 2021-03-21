1715c7a2780bd31273733700ef454dad332ba408
Patrick Coffey <patrickcoffey48@gmail.com>
2021-03-21 18:55:38 -0500

    feat: add refactored max subarray sum

--- src/algos/maxSubarraySum.test.ts
+++ src/algos/maxSubarraySum.test.ts
@@ -1,4 +1,15 @@
-import { maxSubarraySum } from './maxSubarraySum';
+import { maxSubarraySumBruteForce, maxSubarraySum } from './maxSubarraySum';
+
+describe('maxSubarraySumBruteForce', () => {
+  it('finds the maximum sum of n consecutive integers in the given array', () => {
+    expect.assertions(5);
+    expect(maxSubarraySumBruteForce([1, 2, 5, 2, 8, 1, 5], 2)).toBe(10);
+    expect(maxSubarraySumBruteForce([1, 2, 5, 2, 8, 1, 5], 4)).toBe(17);
+    expect(maxSubarraySumBruteForce([4, 2, 1, 6], 1)).toBe(6);
+    expect(maxSubarraySumBruteForce([4, 2, 1, 6, 2], 4)).toBe(13);
+    expect(maxSubarraySumBruteForce([], 4)).toBe(undefined);
+  });
+});
 
 describe('maxSubarraySum', () => {
   it('finds the maximum sum of n consecutive integers in the given array', () => {
--- src/algos/maxSubarraySum.ts
+++ src/algos/maxSubarraySum.ts
@@ -7,7 +7,10 @@
  *
  * @returns maximum sum of n consecutive integers in the given array.
  */
-export function maxSubarraySum(ints: number[], n: number): number | void {
+export function maxSubarraySumBruteForce(
+  ints: number[],
+  n: number
+): number | void {
   if (ints.length <= 0) {
     return;
   }
@@ -29,4 +32,32 @@ export function maxSubarraySum(ints: number[], n: number): number | void {
   }
 
   return max;
+}
+
+/**
+ * Takes an array of integers, and a number, n. Finds the maximum sum of n
+ * consecutive integers in the array.
+ *
+ * @param ints array of numbers.
+ * @param subSize number of consecutive integers to sum.
+ *
+ * @returns maximum sum of n consecutive integers in the given array.
+ */
+export function maxSubarraySum(ints: number[], n: number): number | void {
+  if (ints.length < n) {
+    return;
+  }
+
+  let maxSum = 0;
+  for (let i = 0; i < n; i++) {
+    maxSum += ints[i];
+  }
+
+  let tempSum = maxSum;
+  for (let i = n; i < ints.length; i++) {
+    tempSum = tempSum - ints[i - n] + ints[i];
+    maxSum = maxSum > tempSum ? maxSum : tempSum;
+  }
+
+  return maxSum;
 }
